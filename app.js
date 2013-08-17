af1982a6298c1a6339db9de3b4ae41dc036aa676
John Heenan <j@johnheenan.org>
2013-08-17 12:17:19 +0300

    Added some documentation
    Some comments and a readme

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+//Setup
 var express = require('express');
 var qs = require('querystring');
 
 var app = express();
 
+//Declarations
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
+//Home
 app.get('/', function(req, res) {
 	res.render('index',
 		{ title : 'Home'}
 		);
 });
 
+//Ajax lookup
 app.get('/lookup/:q', function(req, res) {
 	var q = req.params.q;
 	q = qs.unescape(q);
@@ -21,11 +25,13 @@ app.get('/lookup/:q', function(req, res) {
 	res.json(r);
 });
 
+//We're not using a database, just an array
 var lookupStrings = new Array();
 lookupStrings.push("foo");
 lookupStrings.push("bar");
 lookupStrings.push("baz");
 
+//Search function
 function findLookup(s) {
 	var b = false;
 	if(lookupStrings.indexOf(s) != -1) {
@@ -36,5 +42,7 @@ function findLookup(s) {
 	return ret;
 }
 
-app.listen(4000);
-console.log('Server started.');
+//Start
+var port = 4000;
+app.listen(port);
+console.log('Server started on port ' + port);
